df20540ad176b1af1a388add5b60b2059763a317
yibre <cdoyue@gmail.com>
2020-08-24 23:53:16 +0900

    finial application

--- graphql/db.js
+++ graphql/db.js
@@ -1,50 +1,16 @@
-let movies = [
-  {
-    id: 0,
-    name: "Star Wars - The new one",
-    score: 1
-  },
-  {
-    id: 1,
-    name: "Avengers - The new one",
-    score: 8
-  },
-  {
-    id: 2,
-    name: "The Godfather I",
-    score: 99
-  },
-  {
-    id: 3,
-    name: "Logan",
-    score: 2
+import fetch from "node-fetch";
+
+const API_URL = "https://yts.am/api/v2/list_movies.json?"
+
+export const getMovies = (limit, rating) => {
+  let REQUEST_URL = API_URL;
+  if(limit > 0) {
+    REQUEST_URL += `limit=${limit}`;
   }
-];
-// let 은 읽기쓰기전용, const movie로 하면 읽기 전용이 됨
-
-export const getMovies = () => movies;
-  
-export const getById = id => {
-  const filteredMovie = movies.filter(movie => movie.id === id);
-  return filteredMovie[0];
-};
-
-export const deleteMovie = (id) => {
-  const cleanedMovies = movies.filter(movie => movie.id !== id);
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
+  if (rating > 0) {
+    REQUEST_URL += `&minimum_rating=${rating}`;
   }
-};
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: `${movies.length + 1}`,
-    name,
-    score
-  };
-  movies.push(newMovie);
-  return newMovie;
+  return fetch(REQUEST_URL)
+  .then(res=>res.json())
+  .then(json=>json.data.movies);
 };
